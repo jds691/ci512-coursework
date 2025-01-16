b8cb5d1ba14da3fcc239d0acaea81729f7bd565c
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-01-16 15:47:40 +0000

    Stub out breast_cancer.py

---
+++ breast_cancer.py
@@ -0,0 +1,66 @@
+def visualise_dataset() -> None:
+    '''
+
+    :return: None
+    '''
+    # TODO
+    pass
+
+
+def preprocess_data() -> None:
+    '''
+
+    :return: None
+    '''
+    # TODO
+    pass
+
+
+def normalise_data() -> None:
+    '''
+
+    :return: None
+    '''
+    # TODO
+    pass
+
+
+def create_data_split() -> None:
+    '''
+
+    :return: None
+    '''
+    # TODO
+    pass
+
+
+def create_model() -> None:
+    '''
+
+    :return: None
+    '''
+    # TODO
+    pass
+
+
+def run_evaluation() -> None:
+    '''
+
+    :return: None
+    '''
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    # TODO:
+    # - Model training
+    # - Testing and analysis
+    #     - Notable ROC Curve
+
+    visualise_dataset()
+    preprocess_data()
+    normalise_data()
+    create_data_split()
+    create_model()
+    run_evaluation()
